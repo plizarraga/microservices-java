b5a8dc6945be3f326cd84e28383cc4e545b46a69
Pedro Lizarraga <plizarraga86@gmail.com>
2023-07-20 18:43:44 -0700

    removed inventory microservice

--- microservices/inventory-service/src/main/java/com/sapientdemo/inventoryservice/model/dtos/BaseResponse.java
+++
@@ -1,7 +0,0 @@
-package com.sapientdemo.inventoryservice.model.dtos;
-
-public record BaseResponse(String[] errorMessages) {
-    public boolean hasErrors() {
-        return errorMessages != null && errorMessages.length > 0;
-    }
-}
