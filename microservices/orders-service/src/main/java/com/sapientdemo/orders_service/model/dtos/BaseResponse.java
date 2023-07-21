e8b5a3b4b52b345a057c512fe655420ccea3ca77
Pedro Lizarraga <plizarraga86@gmail.com>
2023-07-21 07:46:04 -0700

    Removed orders microservice

--- microservices/orders-service/src/main/java/com/sapientdemo/orders_service/model/dtos/BaseResponse.java
+++
@@ -1,7 +0,0 @@
-package com.sapientdemo.orders_service.model.dtos;
-
-public record BaseResponse(String[] errorMessages) {
-    public boolean hasErrors() {
-        return errorMessages != null && errorMessages.length > 0;
-    }
-}
--- microservices/orders-service/src/main/java/com/sapientdemo/orders_service/model/dtos/OrderItemResponse.java
+++
@@ -1,4 +0,0 @@
-package com.sapientdemo.orders_service.model.dtos;
-
-public record OrderItemResponse(Long id, String sku, Double price, Long quantity) {
-}
